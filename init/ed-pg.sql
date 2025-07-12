bc87db752a136637089fab26b213b83fe48f2faf
HaneenTharwat2 <153605899+HaneenTharwat2@users.noreply.github.com>
2025-07-12 04:53:13 +0300

    Update ed-pg.sql

--- init/ed-pg.sql
+++ init/ed-pg.sql
@@ -1,1 +1,16 @@
+CREATE SCHEMA IF NOT EXISTS test_db;
 
+CREATE TABLE IF NOT EXISTS test_db.users (
+    _id TEXT PRIMARY KEY,
+    data JSONB,
+    createdat TIMESTAMPTZ DEFAULT NOW()
+);
+
+CREATE TABLE IF NOT EXISTS test_db.orders (
+    _id TEXT PRIMARY KEY,
+    data JSONB,
+    createdat TIMESTAMPTZ DEFAULT NOW()
+);
+
+ALTER TABLE test_db.users OWNER TO admin;
+ALTER TABLE test_db.orders OWNER TO admin;
